6da9771f78f890608f5fa98dbdbfa34259fbab60
Kaoru Heanna <kaoru.heanna@trocafone.com>
2018-12-14 12:40:09 -0300

    get device model

---
+++ TfPurchase/ModelCheckViewController.h
@@ -0,0 +1,15 @@
+//
+//  ModelCheckViewController.h
+//  TfPurchase
+//
+//  Created by Kaoru Heanna on 12/14/18.
+//  Copyright © 2018 Kaoru Heanna. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ModelCheckViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UILabel *model;
+
+@end
